0c9e1f9a578e5e032bb5d1604095d1fb98539b59
Ayush Doshi <ayushdoshi1@gmail.com>
2020-12-14 21:32:25 -0500

    Added delete and offset functionality

--- ProgramFiles/AutoSTPtoHDF5Converter.py
+++ ProgramFiles/AutoSTPtoHDF5Converter.py
@@ -4,14 +4,13 @@ import datetime
 import glob
 import logging
 import multiprocessing
+import numpy
 import os
 import shutil
 import subprocess
 import time
 import zipfile
 
-sleeptime = 60
-slowcpusizecutoff = 7e9
 basedir = os.path.dirname(os.getcwd())
 processingpath = os.path.join(basedir, 'Processing')
 
@@ -21,10 +20,24 @@ parser.add_argument('-s', '--system', help='Specify system that you are using. C
                                            'p = Philips Classic, cs = Carescape, pix = Philips PIICiX)', type=str,
                     choices=['u', 'p', 'cs', 'pix'])
 parser.add_argument('-w', '--wave_data', help='Include wavedata', action='store_true')
+parser.add_argument('-l', '--limits', help='List of triples containing (Max Workers, Size limit of files, Timeouts in '
+                                           'hours.)', type=str)
+parser.add_argument('-o', '--offset', help='time string (MM/DD/YYYY) or seconds since 01/01/1970', type=str)
+parser.add_argument('-d', '--delete_stp', help='Delete STP files if conversion if successful', action='store_true')
 args = parser.parse_args()
 
 
-def converter(inputfile):
+def cleanup():
+    zips = glob.glob(os.path.join(processingpath, '**', '*.zip'), recursive=True)
+    hdf = glob.glob(os.path.join(processingpath, '**', '*.hdf5'), recursive=True)
+    xml = glob.glob(os.path.join(processingpath, '**', '*.xml'), recursive=True)
+
+    for path in zips + hdf + xml:
+        if os.path.isfile(path):
+            os.remove(path)
+
+
+def converter(inputfile, timeout):
     processstarttime = time.time()
     inputparentpath = os.path.dirname(inputfile)
     inputfilestructure = os.path.splitext(inputfile)[0]
@@ -54,20 +67,32 @@ def converter(inputfile):
             logger.info('Set to output wavedata as well.')
         if args.system:
             stptoolkitparms[4] = '-' + args.system
-        cmdoutput = subprocess.run(stptoolkitparms, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        cmdoutput = subprocess.run(stptoolkitparms, stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE, text=True, timeout=timeout)
         logger.info(f'{cmdoutput.stdout} \n {cmdoutput.stderr} \n {cmdoutput.returncode}')
 
         logger.info(f'Starting to convert {filebase} from XML to HDF5...')
-        cmdoutput = subprocess.run(
-            ['formatconverter.exe', '-n', '-l', '-t', 'hdf5', '-p', '%d%i.%t', paths[0]],
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        formatconvertparms = ['formatconverter.exe', '-n', '-l', '-t', 'hdf5', '-p', '%d%i.%t', paths[0]]
+        if args.offset:
+            formatconvertparms.insert(3, '--offset')
+            formatconvertparms.insert(4, f'-{args.offset}')
+            formatconvertparms.insert(5, '-a')
+            formatconvertparms.pop(2)
+        cmdoutput = subprocess.run(formatconvertparms, stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE, text=True, timeout=timeout)
         logger.info(
             f'{cmdoutput.stdout} \n {cmdoutput.stderr} \n 'f'{cmdoutput.returncode}')
 
+        if args.offset:
+            old_seconds = filebase.split('-')[1]
+            new_seconds = str(int(old_seconds) - int(args.offset))
+            new_path = paths[3].replace(old_seconds, new_seconds)
+            os.rename(paths[3], new_path)
+
         if not args.no_zip_xml:
             logger.info(f'Zipping up {filebase}.xml...')
-            zipfile.ZipFile(paths[1], mode='w', compression=zipfile.ZIP_DEFLATED,
-                            allowZip64=True, compresslevel=9).write(paths[0], os.path.basename(paths[0]))
+            zipfile.ZipFile(paths[1], mode='w', compression=zipfile.ZIP_DEFLATED, compresslevel=9)\
+                .write(paths[0], os.path.basename(paths[0]))
 
         logger.info(f'Deleting {filebase}.xml file...')
         os.remove(paths[0])
@@ -79,11 +104,12 @@ def converter(inputfile):
             handler.flush()
             handler.close()
 
-        zipfile.ZipFile(paths[6], mode='w', compression=zipfile.ZIP_DEFLATED,
-                        allowZip64=True, compresslevel=9).write(paths[5], os.path.basename(paths[5]))
+        zipfile.ZipFile(paths[6], mode='w', compression=zipfile.ZIP_DEFLATED, compresslevel=9)\
+            .write(paths[5], os.path.basename(paths[5]))
         os.remove(paths[5])
-
-        pathstomove = glob.glob(os.path.join(processingpath, filebase + '.*'))
+        pathstomove = glob.glob(os.path.join(processingpath, filebase + '*.*'))
+        if args.offset:
+            pathstomove.append(new_path)
         for startpath in pathstomove:
             shutil.move(startpath, inputparentpath)
 
@@ -93,41 +119,47 @@ def converter(inputfile):
             logger.removeHandler(handler)
             handler.flush()
             handler.close()
+        paths.pop(5)
+        for path in paths:
+            if os.path.isfile(path):
+                os.remove(path)
         raise
 
     return datetime.timedelta(seconds=time.time() - processstarttime)
 
 
-def run_parallel(parallelfiles, maxworkers):
+def run_parallel(parallelfiles, maxworkers, timeout):
     try:
         print(f'Adding {len(parallelfiles)} to the queue: {"; ".join(parallelfiles)}\n')
-        with concurrent.futures.ProcessPoolExecutor(max_workers=maxworkers) as executor:
+        with concurrent.futures.ProcessPoolExecutor(max_workers=int(maxworkers)) as executor:
             globalstarttime = time.time()
             futures = \
-                {executor.submit(converter, parallelfile): os.path.splitext(os.path.basename(parallelfile))[0]
-                 for parallelfile in parallelfiles}
+                {executor.submit(converter, parallelfile, timeout): parallelfile for parallelfile in parallelfiles}
 
             donecount, timeoutcount, errorcount = 0, 0, 0
             for future in concurrent.futures.as_completed(futures):
+                filename = os.path.splitext(os.path.basename(futures[future]))[0]
                 try:
-                    finishtime = future.result(timeout=10800)
+                    finishtime = future.result()
                     donecount += 1
                     print(f'{str(datetime.timedelta(seconds=time.time() - globalstarttime))}: '
                           f'{str(donecount + timeoutcount + errorcount)}/{str(len(parallelfiles))} finished '
                           f'(Successful: {str(donecount)}, Timeout: {str(timeoutcount)}, Error: {str(errorcount)});'
-                          f' {futures[future]} completed in {finishtime}')
-                except concurrent.futures.TimeoutError:
+                          f' {filename} completed in {finishtime}')
+                    if args.delete_stp:
+                        os.remove(futures[future])
+                except subprocess.TimeoutExpired:
                     timeoutcount += 1
                     print(f'{str(datetime.timedelta(seconds=time.time() - globalstarttime))}: '
                           f'{str(donecount + timeoutcount + errorcount)}/{str(len(parallelfiles))} finished '
                           f'(Successful: {str(donecount)}, Timeout: {str(timeoutcount)}, Error: {str(errorcount)});'
-                          f' {futures[future]} got stuck. Shutting down thread.')
+                          f' {filename} got stuck. Shutting down thread.')
                 except Exception as e:
                     errorcount += 1
                     print(f'{str(datetime.timedelta(seconds=time.time() - globalstarttime))}: '
                           f'{str(donecount + timeoutcount + errorcount)}/{str(len(parallelfiles))} finished '
                           f'(Successful: {str(donecount)}, Timeout: {str(timeoutcount)}, Error: {str(errorcount)});'
-                          f' {futures[future]} got the error "{str(e)}"')
+                          f' {filename} got the error "{str(e)}"')
     except Exception:
         executor.shutdown()
         raise
@@ -135,25 +167,42 @@ def run_parallel(parallelfiles, maxworkers):
 
 if __name__ == '__main__':
     try:
-        inputpath = input('Path to the input folder where I should check for new .stp files (Enter for deafult): ')
+        inputpath = input('Path to the input folder where I should check for .stp files (Enter for default): ')
         if not os.path.isdir(inputpath):
             inputpath = os.path.join(basedir, 'STP_Input')
 
-        foundfiles = [[], []]
-        workerlimit = [multiprocessing.cpu_count() - 2, 10]
+        if not args.limits:
+            limits_string = input('List of tuples that contain the (worker, size in GB, and timeout limits in hours.) ('
+                                  f'Enter for no limits). Max workers available - {multiprocessing.cpu_count() - 1}: ')
+            if limits_string:
+                args.limits = limits_string
+            else:
+                args.limits = '(multiprocessing.cpu_count()-1, numpy.inf, 3)'
+
+        limits = numpy.array(eval(f'[{args.limits}]'))
+        workerlimits, sizelimits, timeouts = limits.T
+        timeouts = (timeouts * 3600)
+
+        filesets = [[]] * len(workerlimits)
+        foundfiles = []
         files = glob.glob(os.path.join(inputpath, '**', '*.?tp'), recursive=True)
         for file in files:
             if not os.path.isfile(os.path.splitext(file)[0] + '.log.zip'):
-                if os.path.getsize(file) < slowcpusizecutoff:
-                    foundfiles[0].append(file)
-                else:
-                    foundfiles[1].append(file)
-        if foundfiles:
-            for index in range(len(foundfiles)):
-                foundfiles[index].sort(key=lambda f: os.stat(f).st_size)
-                run_parallel(foundfiles[index], workerlimit[index])
-        else:
-            print('No .STP files that need to be converted was found.')
+                foundfiles.append(file)
+        sizes = numpy.array([os.path.getsize(file) for file in foundfiles]) / 1e9
+        sizes = numpy.digitize(sizes, sizelimits)
+        foundfiles = numpy.array(foundfiles)
+        for index in range(len(filesets)):
+            filesets[index] = list(foundfiles[sizes == index])
+
+        for idx in range(len(filesets)):
+            if filesets[idx]:
+                print(f'Found {len(filesets[idx])} new .STP files under {sizelimits[idx]}GB.')
+                filesets[idx].sort(key=lambda f: os.stat(f).st_size)
+                run_parallel(filesets[idx], workerlimits[idx], timeouts[idx])
+            else:
+                print(f'No new .STP files found under {sizelimits[idx]}GB.')
+
     except Exception:
         logging.shutdown()
         raise
